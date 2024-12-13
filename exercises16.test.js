e6c1310d1f7158c0f66b85b9304646383f101f17
Mikkel Nygaard Ravn <mikkel@nygaardravn.dk>
2024-12-13 15:46:13 +0100

    Use deepStrictEqual in asserts

--- exercises16.test.js
+++ exercises16.test.js
@@ -4,30 +4,34 @@ import { sum, product } from './exercises16.js';
 
 describe('sum', () => {
     it('should return 0 on empty array', () => {
-        assert.equal(sum([]), 0);
+        assertSame(sum([]), 0);
     });
     it.skip('should return single element of array of length 1', () => {
-        assert.equal(sum([8]), 8);
+        assertSame(sum([8]), 8);
     });
     it.skip('should return sum of two elements', () => {
-        assert.equal(sum([7, 8]), 15);
+        assertSame(sum([7, 8]), 15);
     });
     it.skip('should return sum of three elements', () => {
-        assert.equal(sum([7, -2, 8]), 13);
+        assertSame(sum([7, -2, 8]), 13);
     });
 });
 
 describe('product', () => {
     it.skip('should return 1 on empty array', () => {
-        assert.equal(product([]), 1);
+        assertSame(product([]), 1);
     });
     it.skip('should return single element of array of length 1', () => {
-        assert.equal(product([8]), 8);
+        assertSame(product([8]), 8);
     });
     it.skip('should return product of two elements', () => {
-        assert.equal(product([7, 8]), 56);
+        assertSame(product([7, 8]), 56);
     });
     it.skip('should return product of three elements', () => {
-        assert.equal(product([7, -2, 8]), -112);
+        assertSame(product([7, -2, 8]), -112);
     });
 });
+
+function assertSame(actual, expected) {
+    assert.deepStrictEqual(actual, expected);
+}
